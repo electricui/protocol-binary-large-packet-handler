6f6072d8a8056428e4673e2c295cd175de580520
Michael Orenstein <michael@whiteshadows.me>
2020-11-17 16:08:51 +1030

    chore: Minor cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  globals: {
-    __DEV__: true,
-  },
-  testRegex: '/test/.+(test|spec).[jt]sx?$',
-  moduleFileExtensions: ['ts', 'js'],
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  verbose: true,
-}
+module.exports = require('@electricui/jest-config/jest.config.js')
